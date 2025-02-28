f17b3f52e28d3642846f65248317cf39a180634e
hakimsmox <abdelhakimsmox@gmail.com>
2025-02-28 19:32:29 +0000

    deployment

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,8 +1,9 @@
+export const locales = ["en", "fr"] as const
+export const defaultLocale = "en" as const
+
 import { getRequestConfig } from "next-intl/server"
 
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`./messages/${locale}.json`)).default,
 }))
 
-export const locales = ['en', 'fr'] as const;
-export const defaultLocale = 'en' as const;
